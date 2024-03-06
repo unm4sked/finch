b219b7cbba3457a4b49d20851766ad65bc992085
Lukasz Kalicki <lukasz.kalicki@tsh.io>
2024-03-06 23:17:00 +0100

    chore: refactor responses

--- internal/api/responses/wrapper.go
+++ internal/api/responses/wrapper.go
@@ -4,7 +4,7 @@ func NewSuccessResponseBody[T interface{}](payload T) ResponseWrapper[T] {
 	return ResponseWrapper[T]{
 		Data:    payload,
 		Success: true,
-		Error:   "",
+		Errors:  []string{},
 	}
 }
 
@@ -12,12 +12,12 @@ func NewFailureResponseBody(err error) ResponseWrapper[any] {
 	return ResponseWrapper[any]{
 		Data:    nil,
 		Success: false,
-		Error:   err.Error(),
+		Errors:  []string{err.Error()},
 	}
 }
 
 type ResponseWrapper[T interface{}] struct {
-	Data    T      `json:"data"`
-	Success bool   `json:"success"`
-	Error   string `json:"error"`
+	Data    T        `json:"data"`
+	Success bool     `json:"success"`
+	Errors  []string `json:"errors"`
 }
